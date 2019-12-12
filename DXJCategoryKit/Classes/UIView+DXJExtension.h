4fc2c6f6604d16e0c92fd8171eaecca102bf02ac
AnyaDeng <18701208276@163.com>
2019-12-12 18:23:37 +0800

    新增UIView+Extension分类

---
+++ DXJCategoryKit/Classes/UIView+DXJExtension.h
@@ -0,0 +1,26 @@
+//
+//  UIView+DXJExtension.h
+//  DXJCategoryKit_Example
+//
+//  Created by dxj on 2019/12/12.
+//  Copyright © 2019 AnyaDeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (DXJExtension)
+
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, assign) CGFloat centerX;
+@property (nonatomic, assign) CGFloat centerY;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGSize  size;
+@property (nonatomic, assign) CGPoint origin;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/Pods/Target Support Files/DXJCategoryKit/DXJCategoryKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DXJCategoryKit : NSObject
+@end
+@implementation PodsDummy_DXJCategoryKit
+@end
