155484f96f2927c10f44c3753838b02d4ca06db4
AnyaDeng <18701208276@163.com>
2019-12-12 18:56:35 +0800

    添加依赖

--- Example/Pods/Target Support Files/Pods-DXJCategoryKit_Example/Pods-DXJCategoryKit_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-DXJCategoryKit_Example/Pods-DXJCategoryKit_Example-frameworks.sh
@@ -162,9 +162,15 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/DXJCategoryKit/DXJCategoryKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/DXJTools/DXJTools.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/DXJCategoryKit/DXJCategoryKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/DXJTools/DXJTools.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
